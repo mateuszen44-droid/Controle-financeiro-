2d3553d9f823eef2f1437268a0635633c1e50678
mateuszen44-droid <mateuszen44@gmail.com>
2026-02-25 09:51:37 -0300

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE = "controle-casal-v1";
+const CORE = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(CORE)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(k => (k !== CACHE ? caches.delete(k) : Promise.resolve()))
+    )).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request).then((resp) => {
+        const copy = resp.clone();
+        caches.open(CACHE).then((cache) => cache.put(event.request, copy)).catch(()=>{});
+        return resp;
+      }).catch(()=>cached);
+    })
+  );
+});

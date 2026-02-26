bfebf159f3df2d1b2c70e28ceba0cc0a4ac19b78
mateuszen44-droid <mateuszen44@gmail.com>
2026-02-26 12:00:08 -0300

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,13 @@
-const CACHE = "fmth-v2";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./styles.css",
-  "./app.js",
-  "./manifest.webmanifest"
-];
-
 self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
-});
-
-self.addEventListener("activate", (e) => {
   e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => (k !== CACHE ? caches.delete(k) : null))))
-  );
-});
-
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((cached) => cached || fetch(e.request).then((res) => {
-      const copy = res.clone();
-      caches.open(CACHE).then(cache => cache.put(e.request, copy)).catch(()=>{});
-      return res;
-    }).catch(()=>cached))
+    caches.open("fmth-pro-v1").then((cache) => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./style.css",
+        "./app.js",
+        "./manifest.webmanifest"
+      ]);
+    })
   );
 });

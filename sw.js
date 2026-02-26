e7f4cfd8ec3a1bcb80e0a3b0bec9c21e1fa51baf
mateuszen44-droid <mateuszen44@gmail.com>
2026-02-26 13:27:41 -0300

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,28 @@
-const CACHE = "fmth_cache_v7";
+const CACHE = "fmth-ultra-premium-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.webmanifest"
+];
 
 self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open(CACHE).then((cache) => cache.addAll([
-      "./",
-      "./index.html",
-      "./styles.css",
-      "./app.js",
-      "./manifest.webmanifest"
-    ]))
-  );
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", (e) => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+      Promise.all(keys.map(k => (k !== CACHE ? caches.delete(k) : null)))
     )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
